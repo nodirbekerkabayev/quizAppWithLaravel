ef215a8080678ea2cef6a9f63c21637594d4a708
nodirbekerkabayev <erkabayevnodirbek@gmail.com>
2025-02-07 10:09:44 +0500

    added view pages

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', function () {
+    return view('auth/register');
+});
+
+Route::get('/login', function () {
+    return view('auth/login');
+});
+
+Route::get('/take-quiz', function () {
+    return view('quiz/takeQuiz');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
